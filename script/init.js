1b38086ca324f745233c2ca3177ad9dda921cd27
Ana Clara Venturini <ana146claraventurini@gmail.com>
2021-12-02 13:56:15 -0300

    first commit

---
+++ script/init.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAkOShhtsNZZkA9h1DZFLndKSxSbxgJH70",
+    authDomain: "todo-list-7f549.firebaseapp.com",
+    projectId: "todo-list-7f549",
+    storageBucket: "todo-list-7f549.appspot.com",
+    messagingSenderId: "826281919109",
+    appId: "1:826281919109:web:409eacb85d2599e610dfec"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
